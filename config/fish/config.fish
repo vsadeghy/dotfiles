b0f948910ef629afe42206e06a6bec3712d79abd
vsadeghy <v.sadeghy@gmail.com>
2026-01-08 15:58:00 +0330

    update aliases and add them to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,27 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
 end
 
-set -U fish_greeting
+function import_zsh_aliases --description "Import zsh/bash aliases as fish abbreviations"
+    for line in (rg 'alias \w' "$argv[1]")
+        set -l parts (string split -m1 '=' "$line")
+        set -l name (string trim $parts[1] | string replace 'alias ' '')
+        
+        set -l cmd (string trim "$parts[2]")
+        set cmd (string trim -c "\"'" "$cmd")
+        abbr -a $name "$cmd"
+    end
+end
+
 fish_config theme choose Catppuccin-Macchiato
-fish_vi_key_bindings
+set -g fish_greeting
+set -g fish_key_bindings fish_vi_key_bindings
+set -eU fish_key_bindings
+
+import_zsh_aliases ~/.config/shell/aliasrc.sh
+if command -v pacman &>/dev/null
+    import_zsh_aliases ~/.config/shell/arch.sh
+end
+abbr -a -p anywhere BH -- "--help | bat -plhelp"
 
 pfetch
 starship init fish | source
---
+++ config/shell/arch.sh
@@ -0,0 +1,8 @@
+if [ -f /etc/arch-release ]; then
+    alias in="sudo pacman --needed -S"
+    alias up="sudo pacman --needed -Sy"
+    alias Up="sudo pacman --needed -Syu"
+    alias un="sudo pacman -Rn"
+    alias In="yay --needed -S"
+fi
+
