178bb620054ff33da8733bf0f127d8c9c8c15c10
vsadeghy <v.sadeghy@gmail.com>
2026-07-11 21:46:50 +0330

    update fish source-profile

--- config/fish/conf.d/00-source-profile.fish
+++ config/fish/conf.d/00-source-profile.fish
@@ -1,11 +1,9 @@
-if not status is-interactive
-	exit
-end
+status is-interactive || exit
 
 if not set -q __fish_env_sourced && test -f ~/.profile
-	for line in (bash -c "source ~/.profile; env")
-		set -l env (string split '=' $line)
-		set -Ux $env[1] $env[2]
+	for line in (bash -c "source ~/.profile &>/dev/null; env")
+		set -l env (string split -m1 '=' -- $line)
+		set -Ux $env[1] $env[2] &>/dev/null
 	end
 	set -Ux __fish_env_sourced 1
 end
