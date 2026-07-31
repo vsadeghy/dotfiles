c08e4a1fd90d36de758436a89a996b8545d41101
vsadeghy <v.sadeghy@gmail.com>
2026-07-31 18:58:21 +0330

    fix fish PATH

--- config/fish/conf.d/00-source-profile.fish
+++ config/fish/conf.d/00-source-profile.fish
@@ -3,6 +3,7 @@ status is-interactive || exit
 if not set -q __fish_env_sourced && test -f ~/.profile
 	for line in (bash -c "source ~/.profile &>/dev/null; env")
 		set -l env (string split -m1 '=' -- $line)
+		test $env[1] = "PATH" && set -Ux fish_user_paths $env[2]
 		set -Ux $env[1] $env[2] &>/dev/null
 	end
 	set -Ux __fish_env_sourced 1
